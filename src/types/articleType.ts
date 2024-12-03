2208587ea8f16293f4240652bd3b8a5c0c54cb61
Ada Maynek <ada.maynek@gmail.com>
2024-12-03 23:40:10 +0900

    no message

--- src/types/articleType.ts
+++
@@ -1,8 +0,0 @@
-import { MicroCMSDate, MicroCMSImage } from "microcms-js-sdk";
-
-export type Article = {
-	id: string;
-	title: string;
-	content: string;
-	eyecatch?: MicroCMSImage;
-} & MicroCMSDate;
