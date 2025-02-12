019e73df61098d77194adf444ec4298447b04c80
Ada Maynek <ada.maynek@gmail.com>
2025-02-12 17:29:23 +0900

    no message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <h1 className="font-bold text-3xl mb-1">Web小説</h1>
       <ul >
         <li className="my-2 text-sky-600 no-underline hover:text-red-600 hover:underline">
-          <Link href="/novel/progstars01/">プログラマブル・スターズ ［１］</Link>
+          <Link href="/novel/progstars01/">プログラマブル・スターズ［１］</Link>
         </li>
       </ul>
     </>
