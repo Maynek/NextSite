058cbc3860d89f4d67a81f68ff7751c90a5255f9
Ada Maynek <ada.maynek@gmail.com>
2025-04-10 20:32:17 +0900

    Google Analytics 対応

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,6 +9,11 @@ export default function About() {
     <>
       <h1 className="font-bold text-3xl mb-4">このサイトについて</h1>
       <p>Ada Maynek が執筆している Web 小説 を公開するためのサイトです。</p>
+      <br />
+      <br />
+      <h1 className="font-bold text-3xl mb-4">プライバシーポリシー</h1>
+      <h2 className="font-bold text-2xl mb-4">アクセス解析ツールについて</h2>
+      <p>当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を利用しています。このGoogleアナリティクスはトラフィックデータの収集のためにクッキー（Cookie）を使用しております。トラフィックデータは匿名で収集されており、個人を特定するものではありません。</p>
     </>
   );
 }
